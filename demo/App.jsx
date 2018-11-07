fc6da8466cf06ee55fffdcd22a94eb0da1c2dd3a
wangchong <idoodle@aliyun.com>
2018-11-07 14:13:41 +0800

    add： 把sticky组件加上来

--- demo/App.jsx
+++ demo/App.jsx
@@ -35,10 +35,10 @@ class App extends React.Component {
             labels: 'cta',        // button labels (cta, counts, null)
             language: 'en',       // which language to use (see LANGUAGES)
             networks: [           // which networks to include (see SHARING NETWORKS)
-              'whatsapp',
+              'wechat',
               'linkedin',
               'messenger',
-              'facebook',
+              // 'facebook',
               'twitter'
             ],
             padding: 12,          // padding within buttons (INTEGER)
@@ -56,7 +56,43 @@ class App extends React.Component {
             username: 'custom twitter handle' // (only for twitter sharing)
           }}
         />
-
+        <StickyShareButtons
+          config={{
+            alignment: 'right',    // alignment of buttons (left, right)
+            enabled: true,        // show/hide buttons (true, false)
+            font_size: 16,        // font size for the buttons
+            hide_desktop: false,  // hide buttons on desktop (true, false)
+            labels: 'counts',     // button labels (cta, counts, null)
+            language: 'en',       // which language to use (see LANGUAGES)
+            min_count: 0,         // hide react counts less than min_count (INTEGER)
+            networks: [           // which networks to include (see SHARING NETWORKS)
+              'linkedin',
+              'facebook',
+              'twitter',
+              'pinterest',
+              'email'
+            ],
+            padding: 12,          // padding within buttons (INTEGER)
+            radius: 4,            // the corner radius on each button (INTEGER)
+            show_total: true,     // show/hide the total share count (true, false)
+            show_mobile: true,    // show/hide the buttons on mobile (true, false)
+            show_toggle: true,    // show/hide the toggle buttons (true, false)
+            size: 48,             // the size of each button (INTEGER)
+            top: 160,             // offset in pixels from the top of the page
+      
+            // OPTIONAL PARAMETERS
+            url: 'https://www.sharethis.com', // (defaults to current url)
+            image: 'https://bit.ly/2CMhCMC',  // (defaults to og:image or twitter:image)
+            description: 'custom text',       // (defaults to og:description or twitter:description)
+            title: 'custom title',            // (defaults to og:title or twitter:title)
+            message: 'custom email text',     // (only for email sharing)
+            subject: 'custom email subject',  // (only for email sharing)
+            username: 'custom twitter handle' // (only for twitter sharing)
+      
+          }}
+        />
+        
+        
       </div>
     );
   }
